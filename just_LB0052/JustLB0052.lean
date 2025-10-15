4f6e539f2c926c72978d636cfd8c1d1209642f24
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2025-10-15 16:59:28 +0100

    Bring in files

---
+++ just_LB0052/JustLB0052.lean
@@ -0,0 +1,3 @@
+-- This module serves as the root of the `JustLB0052` library.
+-- Import modules here that should be built as part of the library.
+import «JustLB0052».Basic
---
+++ just_LB0052/lakefile.lean
@@ -0,0 +1,9 @@
+import Lake
+open Lake DSL
+
+package «just_LB0052» where
+  -- add package configuration options here
+
+@[default_target]
+lean_lib «JustLB0052» where
+  -- add library configuration options here
